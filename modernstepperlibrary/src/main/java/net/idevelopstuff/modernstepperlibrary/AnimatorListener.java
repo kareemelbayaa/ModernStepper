6dd63fd8ddd5ae896698cb06b5b970ad22aa4893
Kareem Elbayaa <kareemelbayaa@gmail.com>
2019-10-26 18:26:52 +0300

    Initial commit

---
+++ modernstepperlibrary/src/main/java/net/idevelopstuff/modernstepperlibrary/AnimatorListener.java
@@ -0,0 +1,32 @@
+package net.idevelopstuff.modernstepperlibrary;
+
+
+import android.animation.Animator;
+
+public class AnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation, boolean isReverse) {
+
+    }
+}
